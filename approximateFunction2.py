8af559e706cbd47913b1aa585af4da7ee4341579
John-Anders Stende <jastende@gmail.com>
2016-09-30 12:18:52 +0200

    Finalized general regression class that can save, load and write summaries

--- approximateFunction2.py
+++ approximateFunction2.py
@@ -11,7 +11,6 @@ import shutil
 import matplotlib.pyplot as plt
 from DataGeneration.generateData import functionData
 import neuralNetworkClass as nn
-#import neuralNetworkGeneral as nn
 from Tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 from timeit import default_timer as timer
 
@@ -22,7 +21,23 @@ saveDirName     = ''
 summaryFlag     = False
 summaryDir      = ''
 saveMetaName    = ''
-trainingDir     = 'TrainingData'
+now             = time.datetime.now().strftime("%d.%m-%H.%M.%S")
+trainingDir     = 'TrainingData' + '/' + now
+
+# make directory for training data
+if len(sys.argv) > 1:
+    i = 1
+    while i < len(sys.argv):
+        if sys.argv[i] == '--save' or sys.argv[i] == '--summary':
+            if os.path.exists(trainingDir):
+                print "Attempted to place data in existing directory, %s. Exiting." % trainingDir
+                exit(1)
+            else:
+                os.mkdir(trainingDir)
+                saveMetaName = trainingDir + '/' + 'meta.dat'
+                break
+        i += 1
+        
 
 # process command-line input
 if len(sys.argv) > 1:
@@ -33,24 +48,16 @@ if len(sys.argv) > 1:
             i += 1
             loadFlag     = True
             loadFileName = sys.argv[i]
+            print loadFileName
+            print type(loadFileName)
             
         elif sys.argv[i] == '--save':
             i += 1
             saveFlag = True
             
-            # make new directory if not already made
-            if saveDirName == '':
-                now 			= time.datetime.now().strftime("%d.%m-%H.%M.%S")
-                saveDirName 	= trainingDir + '/' + now
-                
-                # If this directory exists
-                if os.path.exists(saveDirName) :
-                    print "Attempted to place data in existing directory, %s. Exiting." % saveDirName
-                    exit(1)
-                else:
-                    os.mkdir(saveDirName + '/checkpoints')
-                    
-            saveMetaName	= saveDirName + '/' + 'meta.dat'
+            # make new directory for checkpoints
+            saveDirName 	= trainingDir + '/Checkpoints'
+            os.mkdir(saveDirName)
 
             # Copy the python source code used to run the training, to preserve
             # the tf graph (which is not saved by tf.nn.Saver.save()).
@@ -58,18 +65,11 @@ if len(sys.argv) > 1:
             
         elif sys.argv[i] == '--summary':
             i += 1
-            summaryFlag  = True   
-            if saveDirName == '':
-                now 			= time.datetime.now().strftime("%d.%m-%H.%M.%S")
-                saveDirName 	= trainingDir + '/' + now
-                
-                # If this directory exists
-                if os.path.exists(saveDirName) :
-                    print "Attempted to place data in existing directory, %s. Exiting." % \
-                    (saveDirName)
-                    exit(1)
-                else:
-                    os.mkdir(saveDirName + '/summaries')
+            summaryFlag  = True
+            
+            # make new directory for summaries
+            summaryDir = trainingDir + '/Summaries'
+            os.mkdir(summaryDir)
                     
         else:
             i += 1
@@ -88,6 +88,7 @@ class Regression:
 
     def generateData(self, a, b):
         
+        self.a, self.b = a, b        
         self.xTrain, self.yTrain, self.xTest, self.yTest = \
             functionData(self.function, self.trainSize, self.testSize, a, b)
         
@@ -97,8 +98,11 @@ class Regression:
                              
         self.nLayers = nLayers
         self.nNodes  = nNodes
+        self.activation = activation
+        self.wInitMethod = wInitMethod
+        self.bInitMethod = bInitMethod
                       
-        # Input placeholders
+        # input placeholders
         with tf.name_scope('input'):
             self.x = tf.placeholder('float', [None, self.inputs],  name='x-input')
             self.y = tf.placeholder('float', [None, self.outputs], name='y-input')
@@ -139,11 +143,11 @@ class Regression:
         with tf.Session() as sess: 
             
             # pass data to network and receive output
-            self.prediction = self.makeNetwork(x)
+            prediction = self.makeNetwork(x)
             #self.prediction, weights, biases, neurons = self.neuralNetwork(x)
             
             with tf.name_scope('L2Norm'):
-                cost = tf.nn.l2_loss( tf.sub(self.prediction, y) )
+                cost = tf.nn.l2_loss( tf.sub(prediction, y) )
                 tf.scalar_summary('L2Norm', cost/batchSize)
 
             with tf.name_scope('train'):
@@ -158,10 +162,10 @@ class Regression:
                 print 'Model restored'
                 
             # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
-            merged = tf.merge_all_summaries()
-            train_writer = tf.train.SummaryWriter(summaryDir + '/train', sess.graph)
-            test_writer = tf.train.SummaryWriter(summaryDir + '/test')
-            tf.initialize_all_variables().run()
+            if summaryFlag:
+                merged = tf.merge_all_summaries()
+                train_writer = tf.train.SummaryWriter(summaryDir + '/train', sess.graph)
+                test_writer = tf.train.SummaryWriter(summaryDir + '/test')
                     
             # train
             for epoch in xrange(numberOfEpochs):
@@ -170,11 +174,16 @@ class Regression:
                 i = np.random.randint(trainSize-batchSize)
                 xBatch = xTrain[i:i+batchSize]
                 yBatch = yTrain[i:i+batchSize]
-                
+                           
                 # calculate cost on test set every 10th epoch
-                if epoch % 10 == 0:               
-                    summary, testCost = sess.run([merged, cost], feed_dict={x: xTest, y: yTest})
+                if epoch % 100 == 0:               
+                    testCost = sess.run(cost, feed_dict={x: xTest, y: yTest})
+                    print 'Cost/N at step %4d: %g' % (epoch, testCost/testSize)
+                    if summaryFlag:
+                        summary = sess.run(merged, feed_dict={x: xTest, y: yTest})
+                        test_writer.add_summary(summary, epoch)
                 
+                """
                 # run and write meta data every 100th step
                 if epoch % 100 == 99:
                     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
@@ -186,55 +195,43 @@ class Regression:
                     train_writer.add_run_metadata(run_metadata, 'step%4d' % epoch)
                     train_writer.add_summary(summary, epoch)
                     print('Adding run metadata for', epoch)    
+                """
                 
-                # do ordinary train step else
-                else:
-                    summary, trainCost, _ = sess.run([merged, cost, trainStep], feed_dict={x: xBatch, y: yBatch}) 
-                    
-                    
-                    
-                    test_writer.add_summary(summary, epoch)
-                    print 'Cost/N at step %4d: %g' % (epoch, epochCost/testSize)                 
-                    
-                else:          
-
-                    
-
-                        
-                    else:            
-                        summary, _ = sess.run([merged, trainStep], feed_dict={x: xBatch, y: yBatch})
+                # train      
+                trainCost, _ = sess.run([cost, trainStep], feed_dict={x: xBatch, y: yBatch})
+                if summaryFlag:
+                    if epoch % 10 == 0:
+                        summary = sess.run(merged, feed_dict={x: xBatch, y: yBatch})
                         train_writer.add_summary(summary, epoch)
-                
-                      
+                                                        
                 # If saving is enabled, save the graph variables ('w', 'b') and dump
                 # some info about the training so far to SavedModels/<this run>/meta.dat.
-                if saveFlag:
-                    if epoch == 0:
+                                  
+                if epoch == 0:
+                    if saveFlag or summaryFlag:
                         saveEpochNumber = 0
                         with open(saveMetaName, 'w') as outFile:
-                            outStr = '# epochs: %d train: %d, test: %d, batch: %d, nodes: %d, layers: %d' % \
-                                      (numberOfEpochs, trainSize, testSize, batchSize, nNodes, nLayers)
+                            outStr = """# epochs: %d train: %d, test: %d, batch: %d, nodes: %d, layers: %d
+                                        a: %1.1f, b: %1.1f, activation: %s, wInit: %s, bInit: %s""" % \
+                                      (numberOfEpochs, trainSize, testSize, batchSize, nNodes, nLayers, \
+                                       self.a, self.b, self.activation, self.wInitMethod, self.bInitMethod)
                             outFile.write(outStr + '\n')
-                    else:
-                        with open(saveMetaName, 'a') as outFile:
-                            outStr = '%g %g' % (epochLoss/float(trainSize), testCost/float(testSize))
-                            outFile.write(outStr + '\n')
-    
-                    if epoch % 10 == 0:
+                if saveFlag: 
+                    if epoch % 100 == 0:
                         saveFileName = saveDirName + '/' 'ckpt'
                         saver.save(sess, saveFileName, global_step=saveEpochNumber)
-                        saveEpochNumber = saveEpochNumber + 1
-            """            
+                        saveEpochNumber += 1
+                      
             if plot:
-                yy = sess.run(self.prediction, feed_dict={self.x: self.xTest})
+                yy = sess.run(prediction, feed_dict={self.x: self.xTest})
                 plt.plot(self.xTest[:,0], yy[:,0], 'b.')
                 plt.hold('on')
-                xx = np.linspace(a, b, self.testSize)
-                plt.plot(xx, function(xx), 'g-')
+                xx = np.linspace(self.a, self.b, self.testSize)
+                plt.plot(xx, self.function(xx), 'g-')
                 plt.xlabel('r')
                 plt.ylabel('U(r)')
                 plt.legend(['Approximation', 'L-J'])
-                #plt.show()"""
+                plt.show()
                     
         
 
@@ -275,12 +272,17 @@ def performanceTest(maxEpochs, maxLayers, maxNodes):
             
             counter += 1
     
+   
+def LennardJonesExample(trainSize, batchSize, testSize, nLayers, nNodes, nEpochs, a=0.9, b=1.6):
     
-function = lambda s : 1.0/s**12 - 1.0/s**6
-regress = Regression(function, int(1e6), int(1e4), int(1e3))
-regress.generateData(0.9, 1.6)
-regress.constructNetwork(3, 5, activation='Sigmoid', wInitMethod='normal', bInitMethod='normal')
-regress.train(200)
+    function = lambda s : 1.0/s**12 - 1.0/s**6
+    regress = Regression(function, trainSize, batchSize, testSize)
+    regress.generateData(a, b)
+    regress.constructNetwork(nLayers, nNodes, activation='Sigmoid', wInitMethod='normal', bInitMethod='normal')
+    regress.train(nEpochs, plot=False)
+    
+    
+LennardJonesExample(int(1e6), int(1e4), int(1e3), 3, 5, 2000)
 
 
                                                
