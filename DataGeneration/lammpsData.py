Changed scaling and shifting average, was not correct

DataGeneration/lammpsData.py
@@ -31,8 +31,9 @@ def SiTrainingData(dataFolder, symmFuncType, function=None, forces=False, Behler
         Fx = np.array(Fx)
         Fy = np.array(Fy)
         Fz = np.array(Fz)
-        print "Forces is applied"
+        print "Forces are applied"
     else:
+        print 'Forces are not included'
         x, y, z, r, E = readers.readNeighbourData(filename)
     print "Neighbour list file is read..."
 

DataGeneration/symmetries.py
@@ -313,15 +313,6 @@ def applyThreeBodySymmetry(x, y, z, r, parameters, symmFuncType, function=None, 
         sys.stdout.write("\r%2d %% complete" % ((float(i)/size)*100))
         sys.stdout.flush()
         
-        
-    if sampleName:
-        print 
-        print "Writing symmetrized input data to file"
-        with open(sampleName, 'w') as outfile:
-            for vector in inputData:
-                for symmValue in vector:
-                    outfile.write('%g ' % symmValue)
-                outfile.write('\n')
                   
     fractionOfZeros = 1 - fractionOfNonZeros / float(size)
     fractionOfInputVectorsOnlyZeros /= float(size)
@@ -355,16 +346,23 @@ def applyThreeBodySymmetry(x, y, z, r, parameters, symmFuncType, function=None, 
     
     # normalize to [-1,1]
     if normalizeFlag:
-        inputData = 2 * (inputData - minInput) / (maxInput - minInput) - 1 
-        outputData = 2 * (outputData - minOutput) / (maxOutput - minOutput) - 1    
-        print "Normalizing input..."
+        for s in xrange(numberOfSymmFunc):
+            smax = np.max(inputData[:,s])
+            smin = np.min(inputData[:,s])
+            inputData[:,s] = 2 * (inputData[:,s] - smin) / (smax - smin) - 1
+         
+        smax = np.max(outputData[:,0])
+        smin = np.min(outputData[:,0])
+        outputData[:,0] = 2 * (outputData[:,0] - smin) / (smax - smin) - 1            
+        print "Normalizing input and output..."
     
-    if shiftMeanFlag:
-        # shift inputs so that average is zero
-        inputData  -= np.mean(inputData, axis=0)
-        # shift outputs so that average is zero
-        outputData -= np.mean(outputData, axis=0)
-        print "Shifting input mean..."
+    # shift so that average of each symm func is zero over whole training set
+    if shiftMeanFlag:       
+        for s in xrange(numberOfSymmFunc):
+            inputData[:,s] -= np.mean(inputData[:,s])
+            
+        outputData[:,0] -= np.mean(outputData[:,0])
+        print "Shifting input and output mean..."
     
     # scale the covariance
     if scaleCovarianceFlag:
@@ -398,6 +396,15 @@ def applyThreeBodySymmetry(x, y, z, r, parameters, symmFuncType, function=None, 
         print "Min: ", minOutput
         print "Mean: ", np.mean(outputData)
         print
+        
+    if sampleName:
+        print 
+        print "Writing symmetrized input data to file"
+        with open(sampleName, 'w') as outfile:
+            for vector in inputData:
+                for symmValue in vector:
+                    outfile.write('%g ' % symmValue)
+                outfile.write('\n')
     
     return inputData, outputData
     
@@ -623,8 +630,8 @@ def applyThreeBodySymmetryMultiType(x, y, z, r, types, itype, parameters, elem2p
     
     
     
-normalizeFlag       = False    
-shiftMeanFlag       = False
+shiftMeanFlag       = True
+normalizeFlag       = True    
 scaleCovarianceFlag = False
 decorrelateFlag     = False
 

examples.py
@@ -310,12 +310,12 @@ def lammpsTrainingSiO2(nLayers=2, nNodes=10, nEpochs=int(1e5), symmFuncType='G5'
 #                        filename="../LAMMPS_test/Silicon/Data/24.02-16.11.12/neighbours.txt")
 
 """Lammps Stillinger-Weber kjoeringer gir naboer og energier"""
-"""lammpsTrainingSi( nLayers       = 2, 
+lammpsTrainingSi( nLayers       = 2, 
                   nNodes        = 10, 
                   nEpochs       = int(1e5), 
                   activation    = tf.nn.sigmoid, 
                   symmFuncType  = 'G5', 
-                  lammpsDir     = '4Atoms/T1e3N1e4', 
+                  lammpsDir     = 'Bulk/L4T1000N2000Algo', 
                   Behler        = True, 
                   klargerj      = True, 
                   useFunction   = False, 
@@ -323,10 +323,10 @@ def lammpsTrainingSiO2(nLayers=2, nNodes=10, nEpochs=int(1e5), symmFuncType='G5'
                   tags          = False, 
                   batch         = 5, 
                   learningRate  = 0.001, 
-                  RMSEtol       = 0.003 )"""
+                  RMSEtol       = 0.003 )
                   
 """Lammps Stillinger-Weber kjoeringer gir naboer og energier"""
-lammpsTrainingSiO2( nLayers       = 2, 
+"""lammpsTrainingSiO2( nLayers       = 2, 
                     nNodes        = 50, 
                     nEpochs       = int(1e5), 
                     activation    = tf.nn.sigmoid, 
@@ -338,7 +338,7 @@ lammpsTrainingSiO2( nLayers       = 2,
                     forces        = False, 
                     batch         = 5, 
                     learningRate  = 0.001, 
-                    RMSEtol       = 0.003 )
+                    RMSEtol       = 0.003 )"""
                     
 # set numberOfEpochs = -1 when loading network to evalute it
 # set nAtoms to 9 or more to signify training of bulk SiO2, i.e. use bulk symmetry parameters